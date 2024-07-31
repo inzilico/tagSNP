542fbad4382ec52033681a88af9021ce66478205
Gennady Khvorykh <info@inzilico.com>
2024-07-31 06:43:28 +0300

    updated

--- functions.py
+++ functions.py
@@ -7,6 +7,7 @@ import os
 import sys
 import time
 import numpy as np
+import pandas as pd
 
 def check_files(files: list):
     for file in files:
@@ -47,3 +48,49 @@ def bonacich_centrality(A):
     out = [i[0] for i in row_sum_normalized.tolist()]
     # Return 
     return(out)
+
+def code_genotypes(x: str):
+    out = []
+    # Initiate variables
+    with open(x, "r") as f:
+        for line in f:
+            if line.startswith("#"): continue
+            line = line.strip()
+            ar = line.split("\t")
+            # Get ref and alt alleles
+            al = ar[3:5]
+            # Define minor allele
+            vec = []
+            gt = ar[9:]
+            for x in gt:
+                for y in x.split("|"):
+                    vec.append(int(y))
+            freq = sum(vec)/len(vec)             
+            if (freq <= 0.5):
+                minor = al[1]
+                major = al[0]
+                maf = freq
+            else:
+                minor = al[0]
+                major = al[1]
+                maf = 1 - freq
+            # Code genotypes
+            coded = []
+            for x in gt:
+                if x == "0|1" or x == "1|0":
+                    coded.append(0)
+                if x == "0|0" and minor == al[0]:
+                    coded.append(-1)
+                if x == "0|0" and minor == al[1]:
+                    coded.append(1)
+                if x == "1|1" and minor == al[0]:
+                    coded.append(1)
+                if x == "1|1" and minor == al[1]:
+                    coded.append(-1)    
+            out.append([ar[2], ar[1], f'{major}/{minor}', ",".join([str(x) for x in coded]), maf])
+    
+    # Create dataframe
+    #df = pd.DataFrame({"chrom": chrom, "pos": pos, "rs": rs})
+    return(out)
+    
+    
